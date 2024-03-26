0586c90471544fa619a4926baddfa8aa38ec2e20
MizuhoAOKI <mizuhoaoki1998@gmail.com>
2024-03-27 01:03:02 +0900

    [ver 0.0.2] set XY scale as equal

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # csv-plotter
-
 VSCode extension for plotting reference csv for autonomous driving race
+
+## CSV column labels
+`opt_x, opt_y, outer_width, inner_width, center_x, center_y, outer_x, outer_y, inner_x, inner_y, curvature, ref_v`
+
+## Build the package 
+- `cd csv-plotter`
+- `npx vsce package`
+    - `.vsix` file is saved in the workspace
+
+## Install the package with a vsix file
+- Launch VSCode
+- "Ctrl + Shift + X" and open the extension menu
+- Menu `...` -> Select "Install from VSIX" -> specify the vsix file in your PC.
--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as path from 'path';
 
-// Define Webview Content
+// Define webview content as HTML
 function getWebviewContent(scriptUri: any, raceTrackChartConfig: any, velocityChartConfig: any, curvatureChartConfig: any) {
 	return `<!DOCTYPE html>
 	<head>
@@ -37,23 +36,16 @@ function getWebviewContent(scriptUri: any, raceTrackChartConfig: any, velocityCh
 	</head>`;
 }
 
-// This method is called when your extension is activated
+// This method is called when your extension is activated (with Ctrl + Shift + P -> Select "Plot CSV")
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('csv plotter is activated');
-
-	// Say hello
+	// Register the command to plot the CSV file
 	let disposable = vscode.commands.registerCommand('csv-plotter.plot', () => {
 		// The command has been defined in the package.json file
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		// vscode.window.showInformationMessage('csv-plotter is working');
-
-		// create scatter plots
+		// Create scatter plots
 		function createPlotConfig() {
 			// common settings
 			let fontColor = "#999";
@@ -159,6 +151,8 @@ export function activate(context: vscode.ExtensionContext) {
 				aspectRatio: 1,
 				scales: {
 					x: {
+					min: 0,
+					max: 1,
 					type: "linear",
 					position: "bottom",
 					title: {
@@ -171,6 +165,8 @@ export function activate(context: vscode.ExtensionContext) {
 					},
 					},
 					y: {
+					min: 0,
+					max: 1,
 					type: "linear",
 					position: "left",
 					title: {
@@ -187,8 +183,7 @@ export function activate(context: vscode.ExtensionContext) {
 					legend: {
 					display: true,
 					labels: {
-						// show legend only for 0, 1 datasets
-
+						// [TODO] delete legend of focus points
 						color: fontColor,
 					},
 					},
@@ -327,15 +322,14 @@ export function activate(context: vscode.ExtensionContext) {
 			// Load CSV file
 			let editor = vscode.window.activeTextEditor;
 			if (!editor) {
-				// vscode.window.showErrorMessage("No active editor found");
-				return;
+				return; // No active editor found
 			}
 			let doc = editor.document;
 			let focus_position = editor.selection.active; // get cursur position
 			let focus_line = doc.lineAt(focus_position.line); // get current line
 			let text = doc.getText(); // get the whole text
 
-			// parse csv
+			// Parse csv
 			let lines = text.split('\n');
 
 			//// race track plot
@@ -347,9 +341,11 @@ export function activate(context: vscode.ExtensionContext) {
 			let order_focus_outer_bound = 5;
 			let order_inner_bound = 6;
 			let order_focus_inner_bound = 7;
+
 			//// velocity plot
 			let order_velocity = 0;
 			let order_focus_velocity = 1;
+			
 			//// curvature plot
 			let order_curvature = 0;
 			let order_focus_curvature = 1;
@@ -380,8 +376,16 @@ export function activate(context: vscode.ExtensionContext) {
 			let focus_data_curvature = [];
 			let focus_data_velocity = [];
 
+			let min_x_racetrack = +10000.0;
+			let max_x_racetrack = -10000.0;
+			let min_y_racetrack = +10000.0;
+			let max_y_racetrack = -10000.0;
+
 			for (let i = 0; i < lines.length; i++) {
+				// Parse this line
 				let items = lines[i].split(',');
+
+				// Add data
 				data_opt_path.push({x: parseFloat(items[col_opt_path_x]), y: parseFloat(items[col_opt_path_y])});
 				data_center_line.push({x: parseFloat(items[col_center_line_x]), y: parseFloat(items[col_center_line_y])});
 				data_outer_bound.push({x: parseFloat(items[col_outer_bound_x]), y: parseFloat(items[col_outer_bound_y])});
@@ -389,7 +393,13 @@ export function activate(context: vscode.ExtensionContext) {
 				data_curvature.push({x: i, y: parseFloat(items[col_curvature])});
 				data_velocity.push({x: i, y: parseFloat(items[col_velocity])});
 
-				// focus data
+				// Update x y range
+				min_x_racetrack = min_x_racetrack > parseFloat(items[col_outer_bound_x]) ? parseFloat(items[col_outer_bound_x]) : min_x_racetrack;
+				max_x_racetrack = max_x_racetrack < parseFloat(items[col_outer_bound_x]) ? parseFloat(items[col_outer_bound_x]) : max_x_racetrack;
+				min_y_racetrack = min_y_racetrack > parseFloat(items[col_outer_bound_y]) ? parseFloat(items[col_outer_bound_y]) : min_y_racetrack;
+				max_y_racetrack = max_y_racetrack < parseFloat(items[col_outer_bound_y]) ? parseFloat(items[col_outer_bound_y]) : max_y_racetrack;
+
+				// Focus data
 				if (i == focus_line.lineNumber) {
 					focus_data_opt_path.push({x: parseFloat(items[col_opt_path_x]), y: parseFloat(items[col_opt_path_y])});
 					focus_data_center_line.push({x: parseFloat(items[col_center_line_x]), y: parseFloat(items[col_center_line_y])});
@@ -399,6 +409,8 @@ export function activate(context: vscode.ExtensionContext) {
 					focus_data_velocity.push({x: i, y: parseFloat(items[col_velocity])});
 				}
 			}
+
+			// Apply data
 			raceTrackChartConfig.data.datasets[order_opt_path   ].data = data_opt_path;
 			raceTrackChartConfig.data.datasets[order_focus_opt_path].data = focus_data_opt_path;
 			raceTrackChartConfig.data.datasets[order_center_line].data = data_center_line;
@@ -412,53 +424,25 @@ export function activate(context: vscode.ExtensionContext) {
 			curvatureChartConfig.data.datasets[order_curvature].data = data_curvature;
 			curvatureChartConfig.data.datasets[order_focus_curvature].data = focus_data_curvature;
 
+			// Set x, y scale as equal, limiting the range
+			let xy_min = Math.min(min_x_racetrack, min_y_racetrack);
+			let xy_max = Math.max(max_x_racetrack, max_y_racetrack);
+			raceTrackChartConfig.options.scales.x.min = xy_min;
+			raceTrackChartConfig.options.scales.x.max = xy_max;
+			raceTrackChartConfig.options.scales.y.min = xy_min;
+			raceTrackChartConfig.options.scales.y.max = xy_max;
+
+			// Return the plot configurations
 			return [raceTrackChartConfig, velocityChartConfig, curvatureChartConfig];
 		}
 
-		// Show Webview
-        const panel = vscode.window.createWebviewPanel(
-          'csv_plot', // id for the webview used internally
-          'csv_plot', // tab title displayed to the user
-          vscode.ViewColumn.One,
-		  {
-			enableScripts: true,
-		  }
-        );
-		let plotConfig = createPlotConfig();
-		// Get library paths
-		// let libPath = vscode.Uri.file(
-		// 	path.join(
-		// 		context.extensionPath,
-		// 		"node_modules",
-		// 		"chart.js",
-		// 		"dist",
-		// 		"chart.js"
-		// 	)
-		// )
-		
-		let libPath = vscode.Uri.joinPath(
-			context.extensionUri,
-			"node_modules",
-			"chart.js",
-			"dist",
-			"chart.min.js"
-		);
-
-		let scriptUri = panel.webview.asWebviewUri(libPath);
-		console.log(scriptUri);
-
-		// solve plotConfig can be undefined
-		if (plotConfig === undefined) {
-			return;
-		}
-        panel.webview.html = getWebviewContent(scriptUri, plotConfig[0], plotConfig[1], plotConfig[2]);
-
-		// Highlight the current line
+		// Highlight decoration settings
 		let activeEditor = vscode.window.activeTextEditor;
 		let decoration = vscode.window.createTextEditorDecorationType({
 			backgroundColor: 'rgba(220,220,220,.3)'
 		});
 
+		// Update the text decorations
 		function updateDecorations() {
 			if (!activeEditor) {
 				return;
@@ -473,6 +457,27 @@ export function activate(context: vscode.ExtensionContext) {
 			activeEditor.setDecorations(decoration, [range]);
 		}
 
+		// Create Webview
+        const panel = vscode.window.createWebviewPanel(
+			'csv_plot', // id for the webview used internally
+			'csv_plot', // tab title displayed to the user
+			vscode.ViewColumn.One,
+			{
+			  enableScripts: true,
+			}
+		  );
+		  let plotConfig = createPlotConfig();		
+		  let libPath = vscode.Uri.joinPath(
+			  context.extensionUri,
+			  "node_modules",
+			  "chart.js",
+			  "dist",
+			  "chart.min.js"
+		  );
+		  let scriptUri = panel.webview.asWebviewUri(libPath);
+		  console.log(scriptUri);
+
+		// Call this function when the focus is updated on the editor
 		function onFocusUpdate(){
 			// Log
 			console.log('onFocusUpdate');
@@ -488,6 +493,12 @@ export function activate(context: vscode.ExtensionContext) {
 			panel.webview.html = getWebviewContent(scriptUri, plotConfig[0], plotConfig[1], plotConfig[2]);
 		}
 
+		// Solve plotConfig can be undefined
+		if (plotConfig === undefined) {
+			return;
+		}
+        panel.webview.html = getWebviewContent(scriptUri, plotConfig[0], plotConfig[1], plotConfig[2]);
+
 		vscode.window.onDidChangeActiveTextEditor(editor => {
 			activeEditor = editor;
 			if (editor) {
@@ -499,7 +510,8 @@ export function activate(context: vscode.ExtensionContext) {
 			onFocusUpdate();
 		}, null, context.subscriptions);
 
-	});
+	}); // end of registerCommand
+
 	context.subscriptions.push(disposable);
 }
 
